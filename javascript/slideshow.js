4eab21ad541387565d7441c27b8713b08ee18954
ashsax <pipboy1221@gmail.com>
2014-05-26 12:29:28 -0700

    added community links, made tweaks to slideshow

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -1,19 +1,19 @@
 function Slider() {
 				var numImages = $(".slider img").size();
 				var start = 1 + Math.round((numImages - 1)*Math.random());
-				$(".slider #" + start).show("fade", 500);
-				$(".slider #" + start).delay(5500).hide("fade", 500);
+				$(".slider #" + start).show("fade", 250);
+				$(".slider #" + start).delay(2500).hide("fade", 250);
 
 				var count = 2;
 
 				setInterval(function () {
-					$(".slider #" + count).show("fade", 500);
-					$(".slider #" + count).delay(5500).hide("fade", 500);
+					$(".slider #" + count).show("fade", 250);
+					$(".slider #" + count).delay(2500).hide("fade", 250);
 					if (count == numImages) {
 						count = 1;
 					}
 					else {
 						count++;
 					}
-				}, 6500);
+				}, 3000);
 			}
