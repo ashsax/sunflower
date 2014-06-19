2ba3aa7bdc29768a41f2662035388650a6248fb9
ashsax <ashwin.222@gmail.com>
2014-06-18 20:48:50 -0700

    added sidebar to every page

---
+++ javascript/pullout_config.js
@@ -0,0 +1,8 @@
+$(document).ready(function($){
+            $('#pocp1').pullOutContentPanel({
+                pocp_scrollbars : true,
+                pocp_showonload: false,
+                pocp_pg_overlay: false,
+                pocp_clickout: true
+            });
+        });
