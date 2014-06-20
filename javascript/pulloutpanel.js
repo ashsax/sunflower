f88b5e8aa9df887cddb58ec892c04392ce3a4dea
ashsax <ashwin.222@gmail.com>
2014-06-20 13:31:49 -0700

    changed sidebar + sign to "navigate"

--- javascript/pulloutpanel.js
+++ javascript/pulloutpanel.js
@@ -35,7 +35,7 @@ Version : 1.12
         var $element = $(element);
         var element = element;
 
-        $('body').append('<a href="#" class="pocp_button"><i class="icon-plus"></i></a>');
+        $('body').append('<a href="#" class="pocp_button">Navigate</a>');
 
         var pocPanel = $element.children('.pocp'),
             pocPanelButton = $('.pocp_button'),
